c4c06638900384a5293d7e730a06b8336a6686b8
devmustroc <elmustapha.abourar@outlook.com>
2023-08-11 16:26:31 +0200

    :tada:Connect to Redis & Adding Packages

--- utils/redis.js
+++ utils/redis.js
@@ -4,30 +4,33 @@ import util from 'util';
 class RedisClient {
     constructor() {
         this.client = redis.createClient();
-        this.client.on('error', (error) => {
-            console.log(`Redis client not connected to the server: ${error.message}`);
+
+        // Display errors in the console
+        this.client.on('error', (err) => {
+            console.error('Redis Error:', err);
         });
     }
 
     isAlive() {
-        const connected = !this.client.connected;
-        return connected;
+        return !this.client.connected;
     }
 
     async get(key) {
         const getAsync = util.promisify(this.client.get).bind(this.client);
-        const reply = await getAsync(key);
-        return reply;
+        return await getAsync(key);
     }
 
     async set(key, value, duration) {
-        this.client.set(key, value, 'EX', duration);
+        const setAsync = util.promisify(this.client.setex).bind(this.client);
+        await setAsync(key, duration, value);
     }
 
     async del(key) {
-        this.client.del(key);
+        const delAsync = util.promisify(this.client.del).bind(this.client);
+        await delAsync(key);
     }
 }
 
 const redisClient = new RedisClient();
+
 export default redisClient;
