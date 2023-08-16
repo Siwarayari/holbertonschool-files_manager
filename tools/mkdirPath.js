b89c549a921df46554ffccbd883072b77db5d0b3
devmustroc <elmustapha.abourar@outlook.com>
2023-08-16 19:32:43 +0200

    add a new endpoint: to retrieve a data and create a files in DB and Disk

--- tools/mkdirPath.js
+++ tools/mkdirPath.js
@@ -1,14 +1,20 @@
 import fs from 'fs';
+// create file at the root
+// create folder inside a folder
+// create a file at the root
+// create a file inside a folder
 
 const filePath = async (name, data, type) => {
-  const recordPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-  if (!fs.existsSync(recordPath)) fs.mkdirSync(recordPath, { recursive: true });
-  let buff = Buffer.from(data, 'base64');
-  if (type !== 'image') buff = buff.toString('utf-8');
-  fs.writeFile(`${recordPath}/${name}`, buff, (err) => {
+  const dir = process.env.FOLDER_PATH || '/tmp/files_manager';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+  const path = `${dir}/${name}`;
+  let cls = Buffer.from(data, 'base64');
+  if (type !== 'image') cls = cls.toString('utf-8');
+  fs.writeFile(path, cls, (err) => {
     if (err) throw err;
   });
-  return recordPath;
 };
 
 export default filePath;
