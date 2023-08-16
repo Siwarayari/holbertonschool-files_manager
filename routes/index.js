8078f672dcda2ad4bff5405fdd172c5956406499
devmustroc <elmustapha.abourar@outlook.com>
2023-08-16 18:06:20 +0200

    add a new endpoint: to retrieve a data and create a files in DB and Disk

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { getStats, getStatus } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
+import postUpload from '../controllers/FilesController'
 
 const express = require('express');
 
@@ -15,5 +16,6 @@ router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 
 router.post('/users', postNew);
+router.post('/files', postUpload);
 
 export default router;
